079556d88e2a7a53d4f85c94a69a9c4847d315a7
pcariou-dd <46612673+pcariou-dd@users.noreply.github.com>
2019-02-03 16:54:10 +0100

    Update api-list.sh

--- content/api/search/code_snippets/api-list.sh
+++ content/api/search/code_snippets/api-list.sh
@@ -1,6 +1,6 @@
-```javascript
 {
-    query: "my-search:query",
-    time: {from: "now-900s", to: "now"}
+    query: "service:nginx query -method:POST Windows",
+    time: {offset: -3600, from: "2001-01-01 12:00:00", to: "2001-01-01 14:00:00"}
+    sort: "desc",
+    limit: 50 
 }
-```
